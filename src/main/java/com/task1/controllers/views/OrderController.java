dab9dba1ca197d1153dd7d5296ace8182ce719d0
Hamza <hamzamufeed@gmail.com>
2021-09-09 16:43:01 +0300

    init

--- src/main/java/com/task1/controllers/views/OrderController.java
+++ src/main/java/com/task1/controllers/views/OrderController.java
@@ -1,11 +1,12 @@
 package com.task1.controllers.views;
 
 import com.task1.DB.OrderModel;
+import com.task1.controllers.DTOs.OrderDTO;
 import com.task1.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.Map;
 
 @RestController
 public class OrderController {
@@ -14,17 +15,23 @@ public class OrderController {
     OrderService orderService;
 
     @GetMapping("/order/{id}")
-    public Optional<OrderModel> getOrder(@PathVariable int id){
+    public OrderDTO getOrder(@PathVariable int id){
         return orderService.getOrder(id);
     }
 
     @PostMapping("/order/supply")
-    public void supplyOrder(@RequestBody OrderModel orderModel){
-        orderService.supply(orderModel);
+    public void supplyOrder(@RequestBody OrderDTO orderDTO){
+        orderService.supply(orderDTO);
     }
 
-    @PostMapping("/order")
-    public void purchaseOrder(@RequestBody OrderModel orderModel){
-        orderService.purchase(orderModel);
+    @PostMapping("/order/purchase")
+    public void purchaseOrder(@RequestBody OrderDTO orderDTO){
+        orderService.purchase(orderDTO);
+    }
+
+    @GetMapping("/order/history")
+    @ResponseBody
+    public Iterable<OrderModel> getOrderHistory(@RequestParam Map<String,String> allRequestParams){
+        return orderService.getOrderHistory(allRequestParams.toString());
     }
 }
--- src/main/java/com/task1/services/OrderService.java
+++ src/main/java/com/task1/services/OrderService.java
@@ -1,13 +1,11 @@
 package com.task1.services;
 
-import com.task1.DB.AerospikeOrderRepository;
-import com.task1.DB.OrderModel;
-import com.task1.DB.ResourceModel;
+import com.task1.DB.*;
+import com.task1.controllers.DTOs.OrderDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -19,18 +17,25 @@ public class OrderService {
     @Autowired
     TransformerService transformerService;
 
-    public Optional<OrderModel> getOrder(int id){
-        return aerospikeOrderRepository.findById(id);
+    public OrderDTO getOrder(int id){
+        return (OrderDTO) transformerService.EntityToDto((Model) aerospikeOrderRepository.findById(id).get(), OrderDTO.class);    }
+
+    public void supply(OrderDTO orderDTO){
+        //DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        orderDTO.setDate(date);
+        orderDTO.setType("supply");
+        aerospikeOrderRepository.save((OrderModel) transformerService.DtoToEntity((DTO) orderDTO, OrderModel.class));
     }
 
-    public void supply(OrderModel orderModel){ //,Class DtoClass
-        orderModel.setType("supply");
-        aerospikeOrderRepository.save(orderModel);
+    public void purchase(OrderDTO orderDTO){
+        Date date = new Date();
+        orderDTO.setDate(date);
+        orderDTO.setType("purchase");
+        aerospikeOrderRepository.save((OrderModel) transformerService.DtoToEntity((DTO) orderDTO, OrderModel.class));
     }
 
-    public void purchase(OrderModel orderModel){
-        orderModel.setType("purchase");
-        aerospikeOrderRepository.save(orderModel);
+    public Iterable<OrderModel> getOrderHistory(String requestParams) {
+        return aerospikeOrderRepository.findAll();
     }
-
 }
